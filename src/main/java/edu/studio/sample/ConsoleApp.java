5b41862e18b3e10cb86ea03ef59981d8480df5e3
Hector Garcia Boissier <hgarciab@villanova.edu>
2021-04-20 16:56:29 -0400

    Change to test Poll

--- src/main/java/edu/studio/sample/ConsoleApp.java
+++ src/main/java/edu/studio/sample/ConsoleApp.java
@@ -3,9 +3,10 @@ package edu.studio.sample;
 public class ConsoleApp {
 
     public static void main(String[] args) {
-        
+
         System.out.println("Hello Maven");
-        
+        System.out.println("Making a change");
+
     }
 
 }
